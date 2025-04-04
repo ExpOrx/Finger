f737f974403f1c748bf341f71219311aaf7b2586
ExpOrx <1661175532@qq.com>
2025-04-05 00:17:53 +0800

    Update finger_plugin.py

--- finger_plugin.py
+++ finger_plugin.py
@@ -1,135 +1,153 @@
 import idc
 import idaapi
 import idautils
-import re
-import ida_ida
+import traceback
+from finger_sdk import client, ida_func
 
 
-class FuncSigFeature:
+class FingerManager:
     def __init__(self):
-        self.file_path = idc.get_input_file_path()
-
-        self.code_list = ["",".text",".plt",".got","extern",".pdata",".bss"]
-
-        self.control_ins_list = ["call","jc","jnc","jz","jnz","js","jns","jo","jno","jp",
-                                "jpe","jnp","jpo","ja","jnbe","jae","jnb","jb","jnae","jbe",
-                                "jna","je","jne","jg","jnle","jge","jnl","jl","jnge","jle","jng"]
-       
-        self.string_list = dict()
-        for s in idautils.Strings():
-            self.string_list[str(s)] = s.ea
+        self.url = "https://sec-lab.aliyun.com/finger/recognize/"
+        self.headers = {'content-type': 'application/json'}
+        self.timeout = 5
+        self.client = None
 
 
-    def get_file_structure(self):
+    def recognize_function(self, start_ea):
+        func_symbol = None
         try:
-            info = idaapi.get_inf_structure()
-            arch = info.procName
-            if info.is_be():
-                endian = "MSB"
+            self.client = client.Client(self.url, self.headers, self.timeout)
+            func_feat = ida_func.get_func_feature(start_ea)
+            if func_feat:
+                func_id, res = self.client.recognize_function(func_feat)
+                if res and res[func_id]:
+                    func_symbol = res[func_id]
+        except Exception as e:
+            print(traceback.format_exc())
+        if func_symbol:
+            func_symbol = str(func_symbol)  # python2 unicode to str
+        return func_symbol
+
+
+    def recognize_selected_function(self, funcs):
+        for pfn in funcs:
+            func_name = idc.get_func_name(pfn.start_ea)
+            func_symbol = self.recognize_function(pfn.start_ea)
+            if func_symbol:
+                idc.set_color(pfn.start_ea, idc.CIC_FUNC, 0x98FF98)
+                idaapi.set_name(pfn.start_ea, func_symbol, idaapi.SN_FORCE)
+                idaapi.update_func(pfn)
+                print("[+]Recognize %s: %s" %(func_name, func_symbol))
             else:
-                endian = "LSB"
-        except:
-            arch = ida_ida.inf_get_procname()
-            if ida_ida.inf_is_be():
-                endian = "MSB"
+                print("[-]%s recognize failed" %(func_name))
+
+
+    def recognize_function_callback(self, menupath):
+        ea = idaapi.get_screen_ea()
+        pfn = idaapi.get_func(ea)
+        if pfn:
+            func_name = idc.get_func_name(pfn.start_ea)
+            func_symbol = self.recognize_function(pfn.start_ea)
+            if func_symbol:
+                idc.set_color(pfn.start_ea, idc.CIC_FUNC, 0x98FF98)
+                idaapi.set_name(pfn.start_ea, func_symbol, idaapi.SN_FORCE)
+                idaapi.update_func(pfn)
+                print("[+]Recognize %s: %s" %(func_name, func_symbol))
             else:
-                endian = "LSB"
-        return arch, endian
-    
-
-    def get_file_type(self):
-        file_format = ""
-        file_type = ""
-        try:
-            info = idaapi.get_inf_structure()
-            if info.is_64bit():
-                file_format = "64"
-            elif info.is_32bit():
-                file_format = "32"
-            if info.filetype == idaapi.f_PE:
-                file_type = "PE"
-            elif info.filetype == idaapi.f_ELF:
-                file_type = "ELF"
-        except:
-            if ida_ida.inf_is_64bit():
-                file_format = "64"
-            elif ida_ida.inf_is_32bit_exactly() and not ida_ida.inf_is_64bit():
-                file_format = "32"
-            if 	ida_ida.inf_get_filetype() == idaapi.f_PE:
-                file_type = "PE"
-            elif ida_ida.inf_get_filetype() == idaapi.f_ELF:
-                file_type = "ELF"        
-        
-        return file_format, file_type
-
-
-    def get_module_info(self):
-        module_info = ""
-        if len(idc.ARGV) == 2:
-            module_info = idc.ARGV[1]
-        return module_info
-
-
-    def byte2str(self, l):
-        if "bytes" in str(type(l)):
-            l = l.decode()
-        return l
-
-
-    def extract_const(self, ins_addr):
-        const_str = ""
-        op_str = idc.print_insn_mnem(ins_addr)
-        if op_str not in self.control_ins_list:
-            for i in range(2):
-                operand_type = idc.get_operand_type(ins_addr, i)
-                if operand_type == idc.o_mem:
-                    const_addr = idc.get_operand_value(ins_addr, i)
-                    if idc.get_segm_name(const_addr) not in self.code_list:
-                        str_const = idc.get_strlit_contents(const_addr)
-                        if str_const:
-                            str_const = self.byte2str(str_const)
-                            if (str_const in self.string_list) and (const_addr == self.string_list[str_const]):
-                                const_str += str_const
-                                break
-        return const_str
-
-
-    def get_ins_feature(self, start_ea):
-        ins_str_list = list()
-        ins_bytes_list = list()
-        ins_list = list(idautils.FuncItems(start_ea))
-        for ins_addr in ins_list:
-            ins_bytes = idc.get_bytes(ins_addr, idc.get_item_size(ins_addr))
-            ins_bytes_list.append(ins_bytes)
-            ins_str = self.extract_const(ins_addr)
-            ins_str_list.append(ins_str)
-        return ins_bytes_list, ins_str_list
-
-    
-    def filter_segment(self, func_addr):
-        ignore_list = ["extern",".plt",".got",".idata"]
-        if idc.get_segm_name(func_addr) in ignore_list:
-            return True
+                print("[-]%s recognize failed" %(func_name))
         else:
-            return False
+            print("[-]0x%x is not a function" %ea)
 
 
-def get_func_feature(ea):
-    content = dict()
-    pfn = idaapi.get_func(ea)
-    if pfn:
-        func_addr = pfn.start_ea
-        Func = FuncSigFeature()
-        if Func.filter_segment(func_addr):
-            return None
-        arch, endian = Func.get_file_structure()
-        file_format, file_type = Func.get_file_type()
-        module_info = Func.get_module_info()
-        ins_bytes_list, ins_str_list = Func.get_ins_feature(func_addr)
-        content["extmsg"] = [arch, endian, file_format, file_type, module_info]
-        content["ins_bytes"] = ins_bytes_list
-        content["ins_str"] = ins_str_list
-        content["func_name"] = idaapi.get_func_name(func_addr)
-        return content
-    else:
-        return None
+    def recognize_functions_callback(self, menupath):
+        funcs = []
+        for ea in idautils.Functions():
+            funcs.append(idaapi.get_func(ea))
+        self.recognize_selected_function(funcs)
+
+
+class FingerUIManager:
+    class UIHooks(idaapi.UI_Hooks):
+        def finish_populating_widget_popup(self, widget, popup):
+            if idaapi.get_widget_type(widget) == idaapi.BWN_FUNCS:
+                idaapi.attach_action_to_popup(widget, popup, "Finger:RecognizeSelected", "Finger/")
+            if idaapi.get_widget_type(widget) == idaapi.BWN_DISASM:
+                idaapi.attach_action_to_popup(widget, popup, "Finger:RecognizeFunction", "Finger/")
+
+
+    class ActionHandler(idaapi.action_handler_t):
+        def __init__(self, name, label, shortcut=None, tooltip=None, icon=-1, flags=0):
+            idaapi.action_handler_t.__init__(self)
+            self.name = name
+            self.action_desc = idaapi.action_desc_t(name, label, self, shortcut, tooltip, icon, flags)
+
+        def register_action(self, callback,  menupath=None):
+            self.callback = callback
+            if not idaapi.register_action(self.action_desc):
+                return False
+            if menupath and not idaapi.attach_action_to_menu(menupath, self.name, idaapi.SETMENU_APP):
+                return False
+            return True
+
+        def activate(self, ctx):
+            self.callback(ctx)
+
+        def update(self, ctx):
+            return idaapi.AST_ENABLE_ALWAYS
+
+
+    def __init__(self, name):
+        self.name = name
+        self.mgr = FingerManager()
+        self.hooks = FingerUIManager.UIHooks()
+
+    def register_actions(self):
+        menupath = self.name
+        idaapi.create_menu(menupath, self.name, "Help")
+
+        action = FingerUIManager.ActionHandler("Finger:RecognizeFunctions", "Recognize all functions", "")
+        action.register_action(self.mgr.recognize_functions_callback, menupath)
+        action = FingerUIManager.ActionHandler("Finger:RecognizeFunction", "Recognize function", "")
+        action.register_action(self.mgr.recognize_function_callback, menupath)
+        recognize_action = FingerUIManager.ActionHandler("Finger:RecognizeSelected", "Recognize function")
+        if recognize_action.register_action(self.selected_function_callback):
+            self.hooks.hook()
+            return True
+        return False
+
+
+    def selected_function_callback(self, ctx):
+        funcs = map(idaapi.getn_func, ctx.chooser_selection)
+        if ctx.action == "Finger:RecognizeSelected":
+            self.mgr.recognize_selected_function(funcs)
+
+
+def check_ida_version():
+    if idaapi.IDA_SDK_VERSION < 700:
+        print("[-]Finger support 7.x IDA, please update your IDA version.")
+        return False
+    return True
+
+
+class FingerPlugin(idaapi.plugin_t):
+    wanted_name = "Finger"
+    comment, help, wanted_hotkey = "", "", ""
+    flags = idaapi.PLUGIN_FIX | idaapi.PLUGIN_HIDE | idaapi.PLUGIN_MOD
+
+    def init(self):
+        if check_ida_version():
+            idaapi.msg("[+]Finger plugin starts\n")
+            manager = FingerUIManager(FingerPlugin.wanted_name)
+            if manager.register_actions():
+                return idaapi.PLUGIN_OK
+        return idaapi.PLUGIN_SKIP
+
+    def run(self, ctx):
+        return
+
+    def term(self):
+        return
+
+
+def PLUGIN_ENTRY():
+    return FingerPlugin()
